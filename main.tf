ec388243fea2551049f5d5b5ca9f33bf22b97767
Santatra <razafindratsimba34@gmail.com>
2024-06-17 19:26:30 +0300

    change docker -> lxd

--- main.tf
+++ main.tf
@@ -1,31 +1,12 @@
 terraform {
   required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.1"
+    lxd = {
+      source = "terraform-lxd/lxd"
+      version = "2.1.0"
     }
   }
 }
 
-provider "docker" {
-  host     = "ssh://santatra@192.168.56.3:22"
-  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
-}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = true
-  build {
-    context = "."
-    tag     = ["mine:latest"]
-  }
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = var.container_name
-  ports {
-    internal = 80
-    external = 8080
-  }
+provider "lxd" {
+  # Configuration options
 }
